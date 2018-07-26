33e42d77bfc24e33342129745702dd4b10b99b9c
tubunakash <40977763+tubunakash@users.noreply.github.com>
2018-07-26 14:31:21 +0530

    Add files via upload

---
+++ Servant.c
@@ -0,0 +1,16 @@
+    #include<stdio.h>
+    int main()
+    {
+    	int T;
+    	scanf("%d",&T);
+    	while(T--)
+    	{
+    		int N;
+    		scanf("%d",&N);
+    		if(N<10)
+    		printf("What an obedient servant you are!\n");
+    		else
+    		printf("%d\n",-1);
+    	}
+    		return 0;
+    } 
